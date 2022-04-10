b38a1bca467f49fa0d5af4b19af316e883bfcebc
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2022-04-10 10:56:04 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-let Web3 = require('web3');
+import React from 'react';
+import Head from 'next/head';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-function Home() {
-  const [web3, setWeb3] = useState(null)
-  const [address, setAddress] = useState(null)
-  const [contract, setContract] = useState(null)
-  const [isWalletConnected, setWalletConnected] = useState(false)
-
-  let abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "initMessage",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "message",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "newMessage",
-          "type": "string"
-        }
-      ],
-      "name": "update",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ]
-  let contractAddress = "CONTRACT_ADDRESS"
-
-  useEffect(() => {
-    window.ethereum ?
-      ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
-        setAddress(accounts[0])
-        let w3 = new Web3(ethereum)
-        setWeb3(w3)
-        let c = new w3.eth.Contract(abi, contractAddress)
-        setContract(c)
-      }).catch((err) => console.log(err)).finally(setWalletConnected(true))
-      : console.log("Please install MetaMask")
-  }, [])
-
+export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex h-screen justify-center items-center bg-black">
       <Head>
-        <title>NFT Generator</title>
-        <meta name="description" content="NFT Generator" />
+        <title>Elliot Braem</title>
+        <meta name="description" content="Elliot Braem's personal website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          NFT Generator
-        </h1>
-        
-        <p className={styles.description}>
-          { isWalletConnected ? "Generate NFT" : "Please connect MetaMask wallet"}
-        </p>
+      <main className="text-center text-white">
+        <p className="text-xl">Elliot Braem</p>
+        <FontAwesomeIcon className="mx-1" icon={faEnvelope} />
+        <FontAwesomeIcon className="mx-1" icon={faGithub} />
+        <FontAwesomeIcon className="mx-1" icon={faInstagram} />
+        <FontAwesomeIcon className="mx-1" icon={faTwitter} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created by Elliot{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer></footer>
     </div>
-  )
+  );
 }
-
-export default Home
